0457126255d301ccb30e930efce11498dd915971
Javi <javier.candela.berrocal@gmail.com>
2025-12-23 19:26:26 +0100

    Enable static website hosting in S3

--- aws/modules/s3/main.tf
+++ aws/modules/s3/main.tf
@@ -6,3 +6,30 @@ resource "aws_s3_bucket" "webapp_bucket" {
     environment = var.environment
   }
 }
+
+# Define S3 as a static website hosting
+
+resource "aws_s3_bucket_website_configuration" "webapp_config" {
+  bucket = aws_s3_bucket.webapp_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
+
+# Allow public access to S3
+
+resource "aws_s3_bucket_public_access_block" "webapp_access" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+  
+}
