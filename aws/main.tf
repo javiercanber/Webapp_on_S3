26d997de823ff07fb557024e0fee0e8690336602
Javi <javier.candela.berrocal@gmail.com>
2025-12-23 19:07:14 +0100

    Changes on main.tf inside of aws folder

---
+++ aws/main.tf
@@ -0,0 +1,15 @@
+# Provider info
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+}
+
+provider "aws" {
+
+    region = var.aws_region
+}
