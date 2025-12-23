55f8868e9ec57be86a4eef505a7e54a5c205954d
Javi <javier.candela.berrocal@gmail.com>
2025-12-23 18:58:30 +0100

    Reorganization of s3 module

--- aws/modules/s3/variables.tf
+++ aws/modules/s3/variables.tf
@@ -1,10 +1,3 @@
-variable "aws_region" {
-    type = string
-    description = "Region used by AWS"
-    default = "eu-west-1"
-  
-}
-
 variable "environment" {
     type = string
     description = "Tag to identify project environment."
