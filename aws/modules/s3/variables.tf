2db8f7a7a9b6d72bb615776de315d231b5aff92f
Javi <javier.candela.berrocal@gmail.com>
2025-12-23 20:07:46 +0100

    Limpieza de la rama main

--- aws/modules/s3/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {
-    type = string
-    description = "Tag to identify project environment."
-}
-
-variable "project" {
-  type = string
-  description = "Tag to identify name of project."
-}
